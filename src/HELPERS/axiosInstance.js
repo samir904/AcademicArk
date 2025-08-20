de761f38d1d13e1ef3814b117a7ef51f114700aa
samir904 <sumansamir3@gmail.com>
2025-08-21 02:27:39 +0530

    fixed some ui button highlighting issues

--- src/HELPERS/axiosInstance.js
+++ src/HELPERS/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const base_url="http://localhost:5014/api/v1"
-const base_url="https://academicark.onrender.com/api/v1"
+const base_url="http://localhost:5014/api/v1"
+//const base_url="https://academicark.onrender.com/api/v1"
 
 const axiosInstance=axios.create();
 axiosInstance.defaults.baseURL=base_url;
